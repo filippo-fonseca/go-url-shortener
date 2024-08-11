40ce2a20444631f2ff930a598ba064b2310cdeab
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-10 22:12:40 -0600

    initial server

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"sync"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Mapper struct {
+	Mapping map[string]string
+	Lock sync.Mutex
+}
+
+var urlMapper Mapper
+
+func init() {
+	urlMapper = Mapper{
+		Mapping: make(map[string]string),
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is running..."))
+	})
+
+	http.ListenAndServe(":3000", r)
 }
