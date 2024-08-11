ef9000e29a25fc005df0c62d4b4a13a512f57a21
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-10 22:17:27 -0600

    good progress

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"sync"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/lithammer/shortuuid/v4"
 )
 
 type Mapper struct {
@@ -27,5 +30,35 @@ func main() {
 		w.Write([]byte("Server is running..."))
 	})
 
+	r.Post("/short-it", createShortURLHandler)
 	http.ListenAndServe(":3000", r)
+}
+
+func createShortURLHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	u := r.Form.Get("URL")
+
+	if u == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("URL is required"))
+	}
+
+	//generate key
+	key := shortuuid.New()
+
+	//insert
+	insertMapping(key, u)
+
+	log.Println("url mapped successfully")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("http://localhost:3000/short/%s", key)))
+
+	
+}
+
+func insertMapping(key string, u string) {
+	urlMapper.Lock.Lock()
+	defer urlMapper.Lock.Unlock()
+	urlMapper.Mapping[key] = u
 }
